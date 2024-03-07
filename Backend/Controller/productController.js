4117cb225552117169f8805cd07d2a236a87ea52
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2024-03-07 11:58:39 +0530

    created add rating feature

--- Backend/Controller/productController.js
+++ Backend/Controller/productController.js
@@ -2,10 +2,8 @@ const Product = require("../models/productModel")
 
 exports.createProduct = async (req, res) => {
     try {
-        console.log("here")
         const product = await Product.create(req.body)
         await product.save()
-        console.log("here?")
         res.send({ msg: "Product created successfully", product, success: true, status: 200 })
     } catch (error) {
         res.status({ msg: error.message, success: false, status: 400 })
@@ -56,3 +54,23 @@ exports.deleteProductById = async (req, res) => {
         res.send({ msg: error.message, success: false, status: 500 });
     }
 };
+
+exports.addRating = async (req,res)=>{
+    try{
+        const { productId } = req.params;
+        const { star, comment, userId } = req.body;
+        const product = await Product.findById(productId);
+        if (!product) {
+            return res.send({ msg: 'Product not found',success:false,status:404 });
+        }
+        product.ratings.push({ star, comment, postedby: userId });
+        const totalRatings = product.ratings.length;
+        const sumRatings = product.ratings.reduce((total, rating) => total + rating.star, 0);
+        const newTotalRating = sumRatings / totalRatings;
+        product.totalrating = newTotalRating.toFixed(2)
+        await product.save()
+        res.send({msg:"Rating added successfully",success:true,status:200})
+    } catch (error) {
+        res.send({ msg: error.message, success: false, status: 500 });
+    }
+}
--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const router = express.Router()
-const {createProduct,getAllProducts,getProductByID,updateProductById,deleteProductById} = require("../Controller/productController")
+const {createProduct,getAllProducts,getProductByID,updateProductById,deleteProductById,addRating} = require("../Controller/productController")
 
 router.route("/").post(createProduct).get(getAllProducts)
 router.route("/:id").get(getProductByID).put(updateProductById).delete(deleteProductById)
+router.route("/:productId/ratings").post(addRating)
 
 module.exports = router
