2da63e0ecdbd83980491702a29f836b14b35b432
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2024-04-13 12:23:58 +0530

    Refactor cartRoutes.js to use root path for addCart route

--- Backend/routes/cartRoutes.js
+++ Backend/routes/cartRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const {getCart,addCart} = require('../Controller/cartController');
 
 router.get('/:userId', getCart);
-router.post('/add-to-cart', addCart);
+router.post('/', addCart);
 
 module.exports = router;
